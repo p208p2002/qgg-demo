b8461e15aa0015a216b7086b1a0584de49e41257
p208p2002 <p208p2002@gmail.com>
2021-09-19 06:15:54 +0000

    Feat context stride

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ from qgg_utils.optim import GAOptimizer
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from loguru import logger
 
 # server setting
 origins = [    
@@ -29,9 +30,7 @@ app.add_middleware(
 # utils
 def feedback_generation(qgg, input_ids, feedback_times = 3):
         outputs = []
-        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')        
-        input_ids = input_ids.squeeze(0).tolist()        
-        
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
         for i in range(feedback_times):
             gened_text = qgg.tokenizer.bos_token * (len(outputs)+1)
             gened_ids = qgg.tokenizer(gened_text,add_special_tokens=False)['input_ids']            
@@ -95,7 +94,6 @@ async def generate(order:GenerationOrder):
     context = order.context
     question_group_size = order.question_group_size
     candidate_pool_size = order.candidate_pool_size
-    qgg_optim = GAOptimizer(candidate_pool_size,question_group_size)
     
     # 
     if candidate_pool_size < question_group_size:
@@ -105,15 +103,29 @@ async def generate(order:GenerationOrder):
     if question_group_size > 10:
         return {'message':'`question_group_size` must smaller than 10'},400
     
-    input_ids = qgg.tokenizer(
-                    context,
+    tokenize_result = qgg.tokenizer.batch_encode_plus(
+                    [context],
+                    stride=MAX_LENGTH - int(MAX_LENGTH*0.7),
                     max_length=MAX_LENGTH,
                     truncation=True,
                     add_special_tokens=False,
-                    return_tensors='pt'
-                )['input_ids']
-    candidate_questions = feedback_generation(qgg=qgg,input_ids=input_ids,feedback_times=order.candidate_pool_size)
-    question_group = qgg_optim.optimize(candidate_questions,context)
-    return {'question_group':question_group}
+                    return_overflowing_tokens=True,
+                    return_length=True,
+                )
+    candidate_questions = []
+    logger.info(f"Size of tokenize_result.input_ids:{len(tokenize_result.input_ids)}")
+    
+    if len(tokenize_result.input_ids)>=10:
+        logger.warning(f"Force cut tokenize_result.input_ids({len(tokenize_result.input_ids)}) to 10, it's too big")
+        tokenize_result.input_ids = tokenize_result.input_ids[:10]
+        
+    for input_ids in tokenize_result.input_ids:
+        candidate_questions += feedback_generation(qgg=qgg,input_ids=input_ids,feedback_times=order.candidate_pool_size)
+    logger.info(f"Size of candidate_questions:{len(candidate_questions)}")
+
+    while len(candidate_questions) > question_group_size:
+        qgg_optim = GAOptimizer(len(candidate_questions),question_group_size)
+        candidate_questions = qgg_optim.optimize(candidate_questions,context)
+    return {'question_group':candidate_questions}
 
 
